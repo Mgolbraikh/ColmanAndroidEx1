3fd485141298f7f9d1f96ec4375bdc2ea2addb40
Michael Golbraikh <mgolbraikh@gmail.com>
2016-11-12 23:13:40 +0200

    adding the super base class to all the override functions

--- app/src/main/java/com/example/owner/ex1/MainActivity.java
+++ app/src/main/java/com/example/owner/ex1/MainActivity.java
@@ -18,31 +18,37 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onStart() {
+        super.onStart();
         Log.d("tag", "onStarting of activity 1" );
     }
 
     @Override
     protected void onRestart() {
+        super.onRestart();
         Log.d("tag", "OnRestart of activity 1");
     }
 
     protected void  onResume()
     {
+        super.onResume();
         Log.d("tag", "OnResume of activity 1");
     }
 
     @Override
     protected void onPause() {
+        super.onPause();
         Log.d("tag","OnPause of activity 1");
     }
 
     @Override
     protected void onStop() {
+        super.onStop();
         Log.d("tag","onStop of activity 1");
     }
 
     @Override
     protected void onDestroy() {
+        super.onDestroy();
         Log.d("tag", "onDestroy of activity 1");
     }
 }
