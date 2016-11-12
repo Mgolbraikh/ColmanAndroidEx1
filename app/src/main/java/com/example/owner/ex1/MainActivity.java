1d057b252efb9f2f61cc17898af9e7965112baff
Michael Golbraikh <mgolbraikh@gmail.com>
2016-11-12 18:18:41 +0200

    adding all the functions of state of activity

--- app/src/main/java/com/example/owner/ex1/MainActivity.java
+++ app/src/main/java/com/example/owner/ex1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.owner.ex1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +14,35 @@ public class MainActivity extends Activity {
 
         Intent intent = new Intent(this,Activity2.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("tag", "onStarting of activity 1" );
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("tag", "OnRestart of activity 1");
+    }
+
+    protected void  onResume()
+    {
+        Log.d("tag", "OnResume of activity 1");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("tag","OnPause of activity 1");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("tag","onStop of activity 1");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("tag", "onDestroy of activity 1");
     }
 }
